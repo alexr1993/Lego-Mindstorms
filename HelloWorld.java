e629946c6e68fc5735b7456d877d2744eb688199
Alex Remedios <alexr1993@gmail.com>
2015-02-05 19:56:46 +0000

    Add samples

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import lejos.nxt.LCD;
+
+/**
+ * A simple hello world program.
+ * 
+ * @author Lawrie Griffiths
+ */
+public class HelloWorld {
+	public static void main(String[] aArg) throws Exception {
+		LCD.drawString("Hello World", 3, 4);
+		Thread.sleep(2000);
+	}
+}
